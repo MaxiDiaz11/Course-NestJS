470ec6d96c594872581aaca8591d813ae05988b0
Maxi Diaz <maxi.diaz66@gmail.com>
2023-09-02 18:50:00 -0300

    chore(car-dealership): add validationPipe to validate DTO

--- car-dealership/src/cars/cars.service.ts
+++ car-dealership/src/cars/cars.service.ts
@@ -1,18 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CarsService {
-  private cars = [
-    { id: 1, brand: 'Toyota', model: 'Yaris' },
-    { id: 2, brand: 'BMW', model: 'X5' },
-    { id: 3, brand: 'Tesla', model: 'Model 3' },
+  private cars: Car[] = [
+    { id: uuid(), brand: 'Toyota', model: 'Yaris' },
+    { id: uuid(), brand: 'BMW', model: 'X5' },
+    { id: uuid(), brand: 'Tesla', model: 'Model 3' },
   ];
 
   getAllCars() {
     return this.cars;
   }
 
-  getCarById(id: number) {
+  getCarById(id: string) {
     const car = this.cars.find((car) => car.id === id);
 
     if (!car) throw new NotFoundException('Car not found');
--- car-dealership/src/main.ts
+++ car-dealership/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove properties that are not in the DTO
+      forbidNonWhitelisted: true, // throw an error if properties that are not in the DTO are sent
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
